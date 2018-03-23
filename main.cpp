3e07397c4ee874340fd9298260baf7c248d27e87
Amar Jasarbasic <amarjasarbasic@gmail.com>
2018-03-23 00:07:03 -0400

    added init function

--- main.cpp
+++ main.cpp
@@ -4,24 +4,48 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <stdlib.h>
+#include <time.h>
 #include "Matrix.h"
 
-Matrix X, W1, H, W2, Y, B1, Y2, dJdB1, dJdW1, dJdB2, dJdW2;
+using namespace std;
+
+Matrix X, W1, H, W2, Y, B1, B2, Y2, dJdB1, dJdW1, dJdB2, dJdW2;
 double learningRate;
 
 int main() {
     return 0;
 }
+double random(double x)
+{
+    return (double)(rand() % 10000 + 1)/10000-0.5;
+}
 
-void loadTraining(const char *filename, std::vector<std::vector<double> > &input, std::vector<std::vector<double> > &output)
+void init(int inputNeuron, int hiddenNeuron, int outputNeuron, double rate)
+{
+    learningRate = rate;
+
+    W1 = Matrix(inputNeuron, hiddenNeuron);
+    W2 = Matrix(hiddenNeuron, outputNeuron);
+    B1 = Matrix(1, hiddenNeuron);
+    B2 = Matrix(1, outputNeuron);
+
+    W1 = W1.applyFunction(random);
+    W2 = W2.applyFunction(random);
+    B1 = B1.applyFunction(random);
+    B2 = B2.applyFunction(random);
+
+}
+
+void loadTraining(const char *filename, vector<vector<double> > &input, vector<vector<double> > &output)
 {
     int trainingSize = 946;
     input.resize(trainingSize);
     output.resize(trainingSize);
 
-    std::ifstream (file);
+    ifstream (file);
     if (file){
-        std::string line;
+        string line;
         int n;
 
         for (int i=0; i<trainingSize; i++){
@@ -39,6 +63,8 @@ void loadTraining(const char *filename, std::vector<std::vector<double> > &input
     }
     file.close();
 }
+
+
 
 
 
